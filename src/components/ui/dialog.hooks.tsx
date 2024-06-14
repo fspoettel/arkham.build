3a2cd51a896f4dca8d7bdea6a18a4ea28f31f201
Felix Spöttel <1682504+fspoettel@users.noreply.github.com>
2024-06-14 19:20:54 +0200

    refactor: fix HMR for dialog / popovers

---
+++ src/components/ui/dialog.hooks.tsx
@@ -0,0 +1,82 @@
+import {
+  useClick,
+  useDismiss,
+  useFloating,
+  useInteractions,
+  useRole,
+} from "@floating-ui/react";
+import { createContext, useContext, useMemo, useState } from "react";
+
+export interface DialogOptions {
+  initialOpen?: boolean;
+  open?: boolean;
+  onOpenChange?: (open: boolean) => void;
+}
+
+export function useDialog({
+  initialOpen = false,
+  open: controlledOpen,
+  onOpenChange: setControlledOpen,
+}: DialogOptions = {}) {
+  const [uncontrolledOpen, setUncontrolledOpen] = useState(initialOpen);
+  const [labelId, setLabelId] = useState<string | undefined>();
+  const [descriptionId, setDescriptionId] = useState<string | undefined>();
+
+  const open = controlledOpen ?? uncontrolledOpen;
+  const setOpen = setControlledOpen ?? setUncontrolledOpen;
+
+  const data = useFloating({
+    open,
+    onOpenChange: setOpen,
+  });
+
+  const context = data.context;
+
+  const click = useClick(context, {
+    enabled: controlledOpen == null,
+  });
+  const dismiss = useDismiss(context, { outsidePressEvent: "mousedown" });
+  const role = useRole(context);
+
+  const interactions = useInteractions([click, dismiss, role]);
+
+  return useMemo(
+    () => ({
+      open,
+      setOpen,
+      ...interactions,
+      ...data,
+      labelId,
+      descriptionId,
+      setLabelId,
+      setDescriptionId,
+    }),
+    [open, setOpen, interactions, data, labelId, descriptionId],
+  );
+}
+
+type ContextType =
+  | (ReturnType<typeof useDialog> & {
+      setLabelId: React.Dispatch<React.SetStateAction<string | undefined>>;
+      setDescriptionId: React.Dispatch<
+        React.SetStateAction<string | undefined>
+      >;
+    })
+  | undefined;
+
+export const DialogContext = createContext<ContextType>(undefined);
+
+export const useDialogContext = () => {
+  const context = useContext(DialogContext);
+
+  if (context == null) {
+    throw new Error("Dialog components must be wrapped in <Dialog />");
+  }
+
+  return context;
+};
+
+export const useDialogContextUnchecked = () => {
+  const context = useContext(DialogContext);
+  return context;
+};
--- src/components/ui/popover.tsx
+++ src/components/ui/popover.tsx
@@ -1,114 +1,18 @@
-import type { Placement } from "@floating-ui/react";
 import {
   FloatingFocusManager,
   FloatingPortal,
-  autoUpdate,
-  flip,
-  offset,
-  shift,
-  useClick,
-  useDismiss,
-  useFloating,
   useId,
-  useInteractions,
   useMergeRefs,
-  useRole,
 } from "@floating-ui/react";
 import {
   cloneElement,
-  createContext,
   forwardRef,
   isValidElement,
-  useContext,
   useLayoutEffect,
-  useMemo,
-  useState,
 } from "react";
 
-interface PopoverOptions {
-  initialOpen?: boolean;
-  placement?: Placement;
-  modal?: boolean;
-  open?: boolean;
-  onOpenChange?: (open: boolean) => void;
-}
-
-export function usePopover({
-  initialOpen = false,
-  placement = "bottom",
-  modal,
-  open: controlledOpen,
-  onOpenChange: setControlledOpen,
-}: PopoverOptions = {}) {
-  const [uncontrolledOpen, setUncontrolledOpen] = useState(initialOpen);
-  const [labelId, setLabelId] = useState<string | undefined>();
-  const [descriptionId, setDescriptionId] = useState<string | undefined>();
-
-  const open = controlledOpen ?? uncontrolledOpen;
-  const setOpen = setControlledOpen ?? setUncontrolledOpen;
-
-  const data = useFloating({
-    placement,
-    open,
-    onOpenChange: setOpen,
-    whileElementsMounted: autoUpdate,
-    middleware: [
-      offset(5),
-      flip({
-        crossAxis: placement.includes("-"),
-        fallbackAxisSideDirection: "end",
-        padding: 5,
-      }),
-      shift({ padding: 5 }),
-    ],
-  });
-
-  const context = data.context;
-
-  const click = useClick(context, {
-    enabled: controlledOpen == null,
-  });
-  const dismiss = useDismiss(context);
-  const role = useRole(context);
-
-  const interactions = useInteractions([click, dismiss, role]);
-
-  return useMemo(
-    () => ({
-      open,
-      setOpen,
-      ...interactions,
-      ...data,
-      modal,
-      labelId,
-      descriptionId,
-      setLabelId,
-      setDescriptionId,
-    }),
-    [open, setOpen, interactions, data, modal, labelId, descriptionId],
-  );
-}
-
-type ContextType =
-  | (ReturnType<typeof usePopover> & {
-      setLabelId: React.Dispatch<React.SetStateAction<string | undefined>>;
-      setDescriptionId: React.Dispatch<
-        React.SetStateAction<string | undefined>
-      >;
-    })
-  | undefined;
-
-const PopoverContext = createContext<ContextType>(undefined);
-
-export const usePopoverContext = () => {
-  const context = useContext(PopoverContext);
-
-  if (context == null) {
-    throw new Error("Popover components must be wrapped in <Popover />");
-  }
-
-  return context;
-};
+import type { PopoverOptions } from "./popover.hooks";
+import { PopoverContext, usePopover, usePopoverContext } from "./popover.hooks";
 
 export function Popover({
   children,
