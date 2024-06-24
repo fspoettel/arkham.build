df0002e839caa7d80d61f9d4194f234bce7e106f
Felix Spöttel <1682504+fspoettel@users.noreply.github.com>
2024-06-24 17:22:48 +0200

    test(e2e): combine settings tests into one spec

--- e2e/settings-collection.spec.ts
+++ e2e/settings-collection.spec.ts
@@ -1,6 +1,6 @@
 import { expect, test } from "@playwright/test";
 
-test("settings (collection)", async ({ page }) => {
+test("settings: collection", async ({ page }) => {
   await page.goto("/");
 
   await page.getByTestId("masthead-settings").click();
@@ -23,4 +23,24 @@ test("settings (collection)", async ({ page }) => {
   await page.getByTestId("search-input").click();
   await page.getByTestId("search-input").fill("yorick");
   await expect(page.getByTestId("cardlist-count")).toContainText("1 cards");
+});
+
+test("settings: taboo", async ({ page }) => {
+  await page.goto("/");
+
+  await page.getByTestId("search-game-text").click();
+  await page.getByTestId("search-input").fill("Mutated");
+  await expect(page.getByTestId("cardlist-count")).toContainText("0 cards");
+  await page.getByTestId("masthead-settings").click();
+  await page.getByTestId("settings-taboo-set").selectOption("7");
+  await page.getByTestId("settings-save").click();
+  await page.getByTestId("settings-back").click();
+  await expect(page.getByRole("button", { name: "Rex Murphy" })).toBeVisible();
+  await page.getByRole("button", { name: "Rex Murphy" }).click();
+  await expect(page.getByTestId("card-text").first()).toContainText(
+    "Mutated. After you succeed at a skill test by 2 or more while investigating: Discover 1 clue at your location. (Limit once per round.)",
+  );
+  await expect(page.getByTestId("card-taboo").first()).toContainText(
+    "Taboo List Mutated",
+  );
 });
