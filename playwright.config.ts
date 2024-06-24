a7c42e332cd968e35b796e9b5dbadd66431ca6f2
Felix Spöttel <1682504+fspoettel@users.noreply.github.com>
2024-06-24 16:31:03 +0200

    feat: add workflow to test production

--- playwright.config.ts
+++ playwright.config.ts
@@ -70,9 +70,11 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  webServer: {
-    command: "npm run build && npm run preview",
-    url: "http://localhost:3000",
-    reuseExistingServer: !process.env.CI,
-  },
+  webServer: process.env.E2E_BASE_URL
+    ? undefined
+    : {
+        command: "npm run build && npm run preview",
+        url: "http://localhost:3000",
+        reuseExistingServer: !process.env.CI,
+      },
 });
