51c2ab1ff8234b3ba3d30657f72526693d9537de
Felix Spöttel <1682504+fspoettel@users.noreply.github.com>
2024-06-24 15:44:48 +0200

    fix: ignore e2e tests in vitest

--- vite.config.ts
+++ vite.config.ts
@@ -73,6 +73,7 @@ export default defineConfig({
   },
   test: {
     environment: "jsdom",
+    exclude: ["e2e/**", "node_modules/**"],
     setupFiles: "./src/test/setup.ts",
     passWithNoTests: true,
     coverage: {
