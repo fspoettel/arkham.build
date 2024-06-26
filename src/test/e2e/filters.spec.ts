c78bf039fe089f7632d41d501f143eb679ee5371
Felix Spöttel <1682504+fspoettel@users.noreply.github.com>
2024-06-26 10:18:28 +0200

    more tests

---
+++ src/test/e2e/filters.spec.ts
@@ -0,0 +1,50 @@
+import { expect, test } from "@playwright/test";
+import { mockApiCalls } from "./mocks";
+
+test.beforeEach(async ({ page }) => {
+  await mockApiCalls(page);
+  await page.goto("/");
+});
+
+test("faction filter", async ({ page }) => {
+  await page.getByTestId("filters-faction").getByTitle("Seeker").click();
+  await page.getByTestId("search-input").fill("Practice Makes Perfect");
+
+  await expect(page.getByTestId("cardlist-count")).toContainText("1 cards");
+  await expect(page.getByTestId("listcard-06197")).toBeVisible();
+
+  await page.getByTestId("filters-faction").getByTitle("Guardian").click();
+  await page.getByTestId("filters-faction").getByTitle("Seeker").click();
+
+  await expect(page.getByTestId("cardlist-count")).toContainText("0 cards");
+
+  await page.getByTestId("filters-faction").getByTitle("Seeker").click();
+  await expect(page.getByTestId("cardlist-count")).toContainText("1 cards");
+});
+
+test("filter via shortcuts", async ({ page }) => {
+  await page.getByTestId("filters-faction").getByTitle("Multiclass").click();
+
+  await page
+    .getByTestId("filters-type-shortcut")
+    .getByRole("button", { name: "Event" })
+    .click();
+
+  await page
+    .getByTestId("filters-level-shortcut")
+    .getByRole("radio", { name: "Level 1-5" })
+    .click();
+
+  await page
+    .getByTestId("filters-cost")
+    .getByTestId("collapsible-trigger")
+    .click();
+
+  await page.getByLabel("Maximum").click();
+  for (let i = 0; i < 12; i++) {
+    await page.getByLabel("Maximum").press("ArrowLeft");
+  }
+
+  await expect(page.getByTestId("cardlist-count")).toContainText("1 cards");
+  await expect(page.getByTestId("listcard-08087")).toBeVisible();
+});
