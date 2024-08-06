7ea4168f067499017fdae409afd116d48f5a3af9
Felix Spöttel <1682504+fspoettel@users.noreply.github.com>
2024-08-06 18:43:21 +0200

    fix: do not overwrite share state when adding new share

--- src/store/slices/sharing.ts
+++ src/store/slices/sharing.ts
@@ -58,6 +58,7 @@ export const createSharingSlice: StateCreator<
       sharing: {
         ...state.sharing,
         decks: {
+          ...state.sharing.decks,
           [id]: deck.date_update,
         },
       },
