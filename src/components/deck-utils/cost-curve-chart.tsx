79e8b8a6574bb9ae210867ad24cf1c693e94445a
kidManuel <cascallarmanuel@gmail.com>
2024-10-19 18:58:35 -0300

    first pass chart

--- src/components/deck-utils/cost-curve-chart.tsx
+++ src/components/deck-utils/cost-curve-chart.tsx
@@ -1,3 +1,44 @@
-export default function CostCurveChart() {
-  return <div data-testid="card-health">cost</div>;
+import { useMemo } from "react";
+import {
+  VictoryAxis,
+  VictoryChart,
+  VictoryLine,
+  VictoryScatter,
+  VictoryTheme,
+} from "victory";
+
+type Props = {
+  values: { x: number; y: number }[];
+};
+
+export default function CostCurveChart({ values }: Props) {
+  const { maxAmount, tickValues } = useMemo(() => {
+    let maxAmount = 0;
+    const tickValues = [];
+    for (const value of values) {
+      maxAmount = Math.max(value.y, maxAmount);
+      tickValues.push(value.x);
+    }
+
+    return { maxAmount, tickValues };
+  }, [values]);
+
+  return (
+    <div data-testid="card-health">
+      <VictoryChart
+        height={300}
+        theme={VictoryTheme.material}
+        animate={{
+          duration: 550,
+          easing: "expInOut",
+          onLoad: { duration: 150 },
+        }}
+      >
+        <VictoryAxis tickValues={tickValues} />
+        <VictoryAxis dependentAxis domain={[0, maxAmount + 1]} />
+        <VictoryLine data={values} />
+        <VictoryScatter data={values} size={5} />
+      </VictoryChart>
+    </div>
+  );
 }
--- src/components/deck-utils/deck-utils.tsx
+++ src/components/deck-utils/deck-utils.tsx
@@ -2,10 +2,17 @@ import { Suspense, lazy } from "react";
 
 const LazyCostCurveChart = lazy(() => import("./cost-curve-chart"));
 
+const VALUES = [
+  { x: 0, y: 3 },
+  { x: 1, y: 10 },
+  { x: 2, y: 8 },
+  { x: 3, y: 12 },
+];
+
 export function CardUtils() {
   return (
     <Suspense fallback={<div>LOADING</div>}>
-      <LazyCostCurveChart />
+      <LazyCostCurveChart values={VALUES} />
     </Suspense>
   );
 }
