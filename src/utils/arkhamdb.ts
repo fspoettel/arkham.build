005169f4ceb537265a711922ad1f77f18672e9eb
Sirawat Pitaksarit <pyasry@gmail.com>
2024-12-02 17:54:56 +0700

    feat: deck description updates - Nested anchor still get redirected. - Use arkham.build card page if anchor is `/card`. - arkhamdb's `.small` style emulation.

--- src/utils/arkhamdb.ts
+++ src/utils/arkhamdb.ts
@@ -2,11 +2,36 @@ import type { ResolvedDeck } from "@/store/lib/types";
 
 export function redirectArkhamDBLinks(evt: React.MouseEvent) {
   evt.preventDefault();
-  if (evt.target instanceof HTMLAnchorElement) {
-    const href = evt.target.getAttribute("href");
+  const target = evt.target as HTMLElement;
+
+  /**
+   * If one clicked on bolded anchor or image anchor, need to look for `<a>` on its parent.
+   */
+  function searchForAnchorParentRecursive(
+    node: HTMLElement,
+  ): HTMLAnchorElement | null {
+    if (node instanceof HTMLAnchorElement) {
+      return node;
+    }
+    if (node.parentElement) {
+      return searchForAnchorParentRecursive(node.parentElement);
+    }
+    return null;
+  }
+
+  const anchor = searchForAnchorParentRecursive(target);
+  if (anchor !== null) {
+    const href = anchor.getAttribute("href");
     if (!href) return;
 
-    const url = href.startsWith("/") ? `https://arkhamdb.com${href}` : href;
+    let url: string;
+    if (href.startsWith("/card")) {
+      url = href;
+    } else if (href.startsWith("/")) {
+      url = `https://arkhamdb.com${href}`;
+    } else {
+      url = href;
+    }
 
     window.open(url, "_blank");
   }
