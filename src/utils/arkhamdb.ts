d6c062475d0100efe92dfb092d954568a7513907
Felix Spöttel <1682504+fspoettel@users.noreply.github.com>
2024-12-02 12:32:03 +0100

    refactor: replace recursive search with `.closest()`

--- src/utils/arkhamdb.ts
+++ src/utils/arkhamdb.ts
@@ -2,38 +2,25 @@ import type { ResolvedDeck } from "@/store/lib/types";
 
 export function redirectArkhamDBLinks(evt: React.MouseEvent) {
   evt.preventDefault();
-  const target = evt.target as HTMLElement;
 
-  /**
-   * If one clicked on bolded anchor or image anchor, need to look for `<a>` on its parent.
-   */
-  function searchForAnchorParentRecursive(
-    node: HTMLElement,
-  ): HTMLAnchorElement | null {
-    if (node instanceof HTMLAnchorElement) {
-      return node;
+  if (evt.target instanceof HTMLElement) {
+    const anchor = evt.target?.closest("a") as HTMLAnchorElement;
+
+    if (anchor != null) {
+      const href = anchor.getAttribute("href");
+      if (!href) return;
+
+      let url: string;
+      if (href.startsWith("/card")) {
+        url = href;
+      } else if (href.startsWith("/")) {
+        url = `https://arkhamdb.com${href}`;
+      } else {
+        url = href;
+      }
+
+      window.open(url, "_blank");
     }
-    if (node.parentElement) {
-      return searchForAnchorParentRecursive(node.parentElement);
-    }
-    return null;
-  }
-
-  const anchor = searchForAnchorParentRecursive(target);
-  if (anchor !== null) {
-    const href = anchor.getAttribute("href");
-    if (!href) return;
-
-    let url: string;
-    if (href.startsWith("/card")) {
-      url = href;
-    } else if (href.startsWith("/")) {
-      url = `https://arkhamdb.com${href}`;
-    } else {
-      url = href;
-    }
-
-    window.open(url, "_blank");
   }
 }
 
