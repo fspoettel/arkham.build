0861c2a57449a38757468e6431050a694fca5564
Felix Spöttel <1682504+fspoettel@users.noreply.github.com>
2024-12-15 18:32:06 +0100

    fix: disable ownership filter for tdcp

--- src/store/lib/filtering.ts
+++ src/store/lib/filtering.ts
@@ -282,6 +282,8 @@ export function filterOwnership(
   showAllCards: boolean,
 ) {
   return (
+    // FIXME: remove this after tdc release
+    card.pack_code === "tdcp" ||
     ownedCardCount(card, metadata, lookupTables, collection, showAllCards) > 0
   );
 }
