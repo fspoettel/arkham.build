bf1309604304ab577a91dc560f7cc65f7a5b21ef
Felix Spöttel <1682504+fspoettel@users.noreply.github.com>
2024-12-16 12:59:56 +0100

    feat: move taboo change behind tooltip

--- src/components/card/card-taboo-text.tsx
+++ src/components/card/card-taboo-text.tsx
@@ -1,10 +1,15 @@
 import type { Card } from "@/store/services/queries.types";
 import { parseCardTextHtml } from "@/utils/card-utils";
 import { cx } from "@/utils/cx";
+import { DefaultTooltip } from "../ui/tooltip";
 import css from "./card.module.css";
 
 type Props = {
   card: Card;
+};
+
+const TOOLTIP_OPTIONS = {
+  placement: "top-start" as const,
 };
 
 export function CardTabooText(props: Props) {
@@ -15,15 +20,23 @@ export function CardTabooText(props: Props) {
   return (
     <div className={cx("border-taboo", css["text"])} data-testid="card-taboo">
       {!!real_taboo_text_change && (
-        <p>
-          <i className="icon-tablet color-taboo icon-text" /> Taboo List <br />
-          <span
-            // biome-ignore lint/security/noDangerouslySetInnerHtml: trusted origin.
-            dangerouslySetInnerHTML={{
-              __html: parseCardTextHtml(real_taboo_text_change),
-            }}
-          />
-        </p>
+        <DefaultTooltip
+          options={TOOLTIP_OPTIONS}
+          tooltip={
+            <span
+              // biome-ignore lint/security/noDangerouslySetInnerHtml: trusted origin.
+              dangerouslySetInnerHTML={{
+                __html: parseCardTextHtml(real_taboo_text_change),
+              }}
+            />
+          }
+        >
+          <p>
+            <i className="icon-tablet color-taboo icon-text" /> Taboo List{" "}
+            <br />
+            Mutated.
+          </p>
+        </DefaultTooltip>
       )}
       {taboo_xp != null && (
         <p>
