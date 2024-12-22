d2742b629ecec8e5b3a97dba5a00e33abea4815d
Sy Brand <tartanllama@gmail.com>
2024-12-22 22:11:43 +0000

    Don't rerender on subsets of cards to recommend

--- src/components/card-recommender/card-recommender.tsx
+++ src/components/card-recommender/card-recommender.tsx
@@ -8,7 +8,7 @@ import { cx } from "@/utils/cx";
 import { useQuery } from "@/utils/use-query";
 import { useResolvedDeck } from "@/utils/use-resolved-deck";
 import { Rows3Icon } from "lucide-react";
-import { useCallback, useMemo } from "react";
+import { type MutableRefObject, useCallback, useRef } from "react";
 import { Link } from "wouter";
 import { CardList } from "../card-list/card-list";
 import { CardSearch } from "../card-list/card-search";
@@ -21,6 +21,41 @@ import { DeckDateRangeFilter } from "./deck-date-range-filter";
 import { IncludeSideDeckToggle } from "./include-side-deck-toggle";
 import { RecommendationBar } from "./recommendation-bar";
 import { RecommenderRelativityToggle } from "./recommender-relativity-toggle";
+
+// Like useMemo, but has one argument that is allowed to be a subset of previous renders
+// biome-ignore lint/suspicious/noExplicitAny: safe.
+function useMemoSubset<T>(fn: () => T, subsetDep: any, deps: any[]): T {
+  type State = {
+    value: T | null;
+    // biome-ignore lint/suspicious/noExplicitAny: safe.
+    subset: any;
+    // biome-ignore lint/suspicious/noExplicitAny: safe.
+    deps: any[];
+  };
+  const state: MutableRefObject<State | null> = useRef(null);
+
+  const needsRecompute = () => {
+    if (state.current === null) {
+      return true;
+    }
+    if (!subsetDep.every((val) => state.current?.subset.includes(val))) {
+      return true;
+    }
+    if (!state.current.deps.every((val, idx) => val === deps[idx])) {
+      return true;
+    }
+  };
+
+  if (needsRecompute()) {
+    state.current = {
+      value: fn(),
+      subset: subsetDep,
+      deps: deps,
+    };
+  }
+
+  return state.current?.value as T;
+}
 
 export function CardRecommender(props: CardListProps) {
   const {
@@ -47,39 +82,43 @@ export function CardRecommender(props: CardListProps) {
   const dateRange = useStore((state) => state.recommender.deckFilter);
 
   const coreCards = useStore((state) => state.recommender.coreCards);
-  const recommendationQuery = useMemo(() => {
-    if (!resolvedDeck?.id || !listState) {
-      return () => Promise.resolve({ recommendations: [], decks_analyzed: 0 });
-    }
-    const dateRangeStrings = dateRange.map(deckTickToString) as [
-      string,
-      string,
-    ];
-    // We don't want to recommend signatures, story cards, or weaknesses
-    const toRecommend = listState.cards
-      .filter((card) => card.xp != null)
-      .map((card) => card.code);
-    const canonicalizedInvestigatorCode = `${resolvedDeck?.metaParsed.alternate_back ?? resolvedDeck?.investigator_code}-${resolvedDeck?.metaParsed.alternate_front ?? resolvedDeck?.investigator_code}`;
-    return () =>
-      getRecommendations(
-        canonicalizedInvestigatorCode,
-        includeSideDeck,
-        isRelative,
-        coreCards[resolvedDeck.id] || [],
-        toRecommend,
-        dateRangeStrings,
-      );
-  }, [
-    resolvedDeck?.id,
-    resolvedDeck?.investigator_code,
-    resolvedDeck?.metaParsed.alternate_back,
-    resolvedDeck?.metaParsed.alternate_front,
-    includeSideDeck,
-    isRelative,
-    listState,
-    dateRange,
-    coreCards,
-  ]);
+  const recommendationQuery = useMemoSubset(
+    () => {
+      if (!resolvedDeck?.id || !listState?.cards) {
+        return () =>
+          Promise.resolve({ recommendations: [], decks_analyzed: 0 });
+      }
+      const dateRangeStrings = dateRange.map(deckTickToString) as [
+        string,
+        string,
+      ];
+      // We don't want to recommend signatures, story cards, or weaknesses
+      const toRecommend = listState.cards
+        .filter((card) => card.xp != null)
+        .map((card) => card.code);
+      const canonicalizedInvestigatorCode = `${resolvedDeck?.metaParsed.alternate_back ?? resolvedDeck?.investigator_code}-${resolvedDeck?.metaParsed.alternate_front ?? resolvedDeck?.investigator_code}`;
+      return () =>
+        getRecommendations(
+          canonicalizedInvestigatorCode,
+          includeSideDeck,
+          isRelative,
+          coreCards[resolvedDeck.id] || [],
+          toRecommend,
+          dateRangeStrings,
+        );
+    },
+    listState?.cards, //Allows the new version of listState.cards to be a subset of the old one
+    [
+      resolvedDeck?.id,
+      resolvedDeck?.investigator_code,
+      resolvedDeck?.metaParsed.alternate_back,
+      resolvedDeck?.metaParsed.alternate_front,
+      includeSideDeck,
+      isRelative,
+      dateRange,
+      coreCards,
+    ],
+  );
 
   const { data, state } = useQuery(recommendationQuery);
 
