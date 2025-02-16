882e96eff985bb2f3eb5f0a257884209c41f1fc0
Felix Spöttel <1682504+fspoettel@users.noreply.github.com>
2025-02-16 16:51:10 +0100

    test: add e2e test

--- src/pages/settings/show-move-to-side-deck.tsx
+++ src/pages/settings/show-move-to-side-deck.tsx
@@ -1,6 +1,6 @@
 import { Checkbox } from "@/components/ui/checkbox";
 import { Field } from "@/components/ui/field";
-import { Trans, useTranslation } from "react-i18next";
+import { useTranslation } from "react-i18next";
 import type { SettingProps } from "./types";
 
 export function ShowMoveToSideDeckSetting(props: SettingProps) {
@@ -17,16 +17,12 @@ export function ShowMoveToSideDeckSetting(props: SettingProps) {
   return (
     <Field
       bordered
-      helpText={
-        <Trans i18nKey={"settings.display.show_move_to_side_deck_help"} t={t}>
-          When this is checked a button to move cards to the side deck is
-          displayed.
-        </Trans>
-      }
+      helpText={t("settings.display.show_move_to_side_deck_help")}
     >
       <Checkbox
         checked={settings.showMoveToSideDeck}
         label={t("settings.display.show_move_to_side_deck")}
+        data-testid="show-move-to-side-deck"
         id="show-move-to-side-deck"
         name="show-move-to-side-deck"
         onCheckedChange={onCheckMoveToSideDeckSetting}
