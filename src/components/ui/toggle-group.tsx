2edca945668f71e2470611ca33f09f9caf69efe4
Felix Spöttel <1682504+fspoettel@users.noreply.github.com>
2025-03-13 19:02:11 +0100

    feat: hold shift to use "exclusive" toggle mode

--- src/components/ui/toggle-group.tsx
+++ src/components/ui/toggle-group.tsx
@@ -5,7 +5,7 @@ import type {
   ToggleGroupSingleProps,
 } from "@radix-ui/react-toggle-group";
 import { Item, Root } from "@radix-ui/react-toggle-group";
-import { forwardRef } from "react";
+import { forwardRef, useCallback, useEffect, useRef } from "react";
 import css from "./toggle-group.module.css";
 import { Tooltip, TooltipContent, TooltipTrigger } from "./tooltip";
 
@@ -15,14 +15,55 @@ type ToggleGroupProps = (ToggleGroupSingleProps | ToggleGroupMultipleProps) & {
 };
 
 export function ToggleGroup({
+  className,
   full,
   icons,
-  className,
+  onValueChange,
+  type,
+  value,
   ...rest
 }: ToggleGroupProps) {
+  const shiftKeyPressed = useRef(false);
+
+  useEffect(() => {
+    function handleKeyDown(e: KeyboardEvent) {
+      if (e.key === "Shift") shiftKeyPressed.current = true;
+    }
+
+    function handleKeyUp(e: KeyboardEvent) {
+      if (e.key === "Shift") shiftKeyPressed.current = false;
+    }
+
+    document.addEventListener("keydown", handleKeyDown);
+    document.addEventListener("keyup", handleKeyUp);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+      document.removeEventListener("keyup", handleKeyUp);
+    };
+  }, []);
+
+  const onValueChangeBound = useCallback(
+    (val: string | string[]) => {
+      if (type === "multiple" && shiftKeyPressed.current) {
+        const newValue = (val as string[]).filter((v) => !value?.includes(v));
+        onValueChange?.(newValue);
+      } else {
+        // biome-ignore lint/suspicious/noExplicitAny: work around library type.
+        onValueChange?.(val as any);
+      }
+    },
+    [onValueChange, type, value],
+  );
+
   return (
     <Root
       {...rest}
+      // biome-ignore lint/suspicious/noExplicitAny: work around library type.
+      value={value as any}
+      onValueChange={onValueChangeBound}
+      // biome-ignore lint/suspicious/noExplicitAny: work around library type.
+      type={type as any}
       className={cx(
         css["togglegroup"],
         className,
