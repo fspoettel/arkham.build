e2dc54255f21b3eea6f78497292084bbb649e741
Felix Spöttel <1682504+fspoettel@users.noreply.github.com>
2025-03-27 19:34:51 +0100

    fix: localized data version query

--- src/app.tsx
+++ src/app.tsx
@@ -153,6 +153,7 @@ function RouteReset() {
 function AppTasks() {
   const dataVersion = useStore((state) => state.metadata.dataVersion);
   const connections = useStore((state) => state.connections);
+  const locale = useStore((state) => state.settings.locale);
 
   const sync = useSync();
   const toast = useToast();
@@ -168,7 +169,7 @@ function AppTasks() {
       if (cardDataLock.current) return;
       cardDataLock.current = true;
 
-      const data = await queryDataVersion();
+      const data = await queryDataVersion(locale);
 
       const upToDate =
         data &&
@@ -177,6 +178,8 @@ function AppTasks() {
         data.cards_updated_at === dataVersion.cards_updated_at &&
         data.translation_updated_at === dataVersion.translation_updated_at &&
         data.version === dataVersion.version;
+
+      console.log(data, dataVersion);
 
       if (!upToDate && !toastId.current) {
         toastId.current = toast.show({
@@ -204,7 +207,7 @@ function AppTasks() {
     ) {
       updateCardData().catch(console.error);
     }
-  }, [dataVersion, toast.dismiss, toast.show, location]);
+  }, [dataVersion, toast.dismiss, toast.show, location, locale]);
 
   const autoSyncLock = useRef(false);
 
